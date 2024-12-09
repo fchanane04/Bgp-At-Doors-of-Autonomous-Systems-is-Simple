66cd5d6179d2e050b81bff5a9c8147aa930e4f98
root <root@bgp.myguest.virtualbox.org>
2024-12-09 19:54:15 +0100

    added d-bus installation

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#installation of GNS3
+add-apt-repository ppa:gns3/ppa
+apt update
+apt install gnome-session-flashback
+apt install gns3-gui
+
+#installation of docker
+apt install docker.io
+usermod -aG docker $USER
+
+#install D-Bus tool to avoid dbus-launch error
